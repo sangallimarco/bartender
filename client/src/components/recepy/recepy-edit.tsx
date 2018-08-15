4fcf4f42ce4bb6659d322f948e72826360b084d2
marco sangalli <sangalli.marco@gmail.com>
2018-08-15 11:51:22 +0200

    save hook

--- client/src/components/recepy/recepy-edit.tsx
+++ client/src/components/recepy/recepy-edit.tsx
@@ -8,6 +8,7 @@ import { Input } from '../input/input';
 // import { RoutePath } from '../../shared';
 import './recepy-edit.css';
 import { generateRangeFromEnumKeys } from '../../core/enum-utils';
+import { browserHistory } from '../../core/browser-history';
 
 interface RecepyEditProps extends RouteComponentProps<any> {
     id: string;
@@ -46,22 +47,20 @@ export class RecepyEdit extends BaseComponent<RecepyEditProps, RecepyEditState> 
                 this.setState({ families });
             })
         );
-
-        // this.listeners.push(
-        //     webSocketService.on<RecepyPayload>(RoutePath.EDIT, (data) => {
-        //         const { recepy } = data;
-        //         this.setState({ recepy });
-        //     })
-        // );
+        this.listeners.push(
+            webSocketService.on<{}>(RoutePath.EDIT, (data) => {
+                browserHistory.push('/');
+            })
+        );
 
         webSocketService.send<GetPayload>(RoutePath.GET, { id });
         webSocketService.send<{}>(RoutePath.GET_FAMILIES, {});
     }
 
     public render() {
-        const { recepy: { label, parts }, families, loaded } = this.state;
+        const { recepy: { id, label, parts }, families, loaded } = this.state;
         return loaded && <div className="recepy-edit">
-            <Input name="label" value={label} onChange={this.handleChange} />
+            <Input name="id" value={id} onChange={this.handleChange} />
             <Input name="label" value={label} onChange={this.handleChange} />
             {this.renderPumps(parts)}
             <Button onClick={this.handleSubmit} type={ButtonType.ACTION}>SAVE</Button>
@@ -94,7 +93,7 @@ export class RecepyEdit extends BaseComponent<RecepyEditProps, RecepyEditState> 
         const { target: { value, name } } = e;
         const indx = +name;
         const quantity = +value;
-        const {parts} = prevRecepy;
+        const { parts } = prevRecepy;
         parts.splice(indx, 1, quantity);
         const recepy: Recepy = { ...prevRecepy, parts };
         this.setState({ recepy })
