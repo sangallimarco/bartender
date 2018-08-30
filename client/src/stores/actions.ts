673c03ea34c6a2f1dfe2ebac541e1856d04fdd07
Marco Sangalli <msangalli@sapient.com>
2018-08-30 16:09:41 +0200

    fixing action

--- client/src/stores/actions.ts
+++ client/src/stores/actions.ts
@@ -1,13 +1,11 @@
 import { createAction } from 'typesafe-actions';
 import { EDIT, RECEPIES, CMD_RECEPIES, CMD_FAMILIES, FAMILIES, CMD_MAKE, CMD_EDIT, MAKE, RecepyPayload, RecepiesPayload, ProcessingPayload, RecepyFamiliesPayload, Recepy, SET_RECEPY, SET_PART, AttributePayload, SET_ATTRIBUTE, CMD_NEW, NEW, CMD_DELETE } from '../types';
 import { ActionType } from 'typesafe-actions';
-import { webSocketService } from '../core/websocket';
 
 export const RootActions = {
     [CMD_RECEPIES]: createAction(CMD_RECEPIES, resolve => {
         return () => {
-            webSocketService.send(CMD_RECEPIES, {});
-            return resolve({})
+            return resolve({});
         };
     }),
     [RECEPIES]: createAction(RECEPIES, resolve => {
--- client/src/stores/reducer.ts
+++ client/src/stores/reducer.ts
@@ -1,5 +1,5 @@
 import { Reducer } from 'redux';
-import { Recepy, RecepyFamily, MakePayload, CMD_MAKE, CMD_FAMILIES, CMD_EDIT, CMD_NEW, CMD_DELETE } from '../types';
+import { Recepy, RecepyFamily, MakePayload, CMD_MAKE, CMD_FAMILIES, CMD_EDIT, CMD_NEW, CMD_DELETE, CMD_RECEPIES } from '../types';
 import { RootAction, RootActions } from './actions';
 import { webSocketService } from '../core/websocket';
 import { getType } from 'typesafe-actions';
@@ -25,9 +25,9 @@ export const reducer: Reducer<RootReducerState> = (
     action: RootAction
 ): RootReducerState => {
     switch (action.type) {
-        // case getType(RootActions.CMD_RECEPIES):
-        //     webSocketService.send(CMD_RECEPIES, {});
-        //     return state;
+        case getType(RootActions.CMD_RECEPIES):
+            webSocketService.send(CMD_RECEPIES, {});
+            return state;
         case getType(RootActions.RECEPIES):
             const { recepies } = action.payload;
             return { ...state, recepies };
