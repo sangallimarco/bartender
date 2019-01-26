aa5f7e9fc7c438008ed716c758515617a29fba87
Marco Sangalli <msangalli@sapient.com>
2019-01-26 14:06:06 +0100

    prebuild

---
+++ client/build/precache-manifest.d5ed3f55a0aa47ed7c2f9f9645c3820c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e9bc2ac206e57e208954",
+    "url": "/static/js/main.e9bc2ac2.chunk.js"
+  },
+  {
+    "revision": "ed951a24988cb5276d20",
+    "url": "/static/js/1.ed951a24.chunk.js"
+  },
+  {
+    "revision": "e9bc2ac206e57e208954",
+    "url": "/static/css/main.f169815b.chunk.css"
+  },
+  {
+    "revision": "ece24fc2a13c998adca96f9481647a07",
+    "url": "/index.html"
+  }
+];
