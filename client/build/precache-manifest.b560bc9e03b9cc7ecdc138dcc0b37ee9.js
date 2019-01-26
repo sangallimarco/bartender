2db7ca898ed9d2af342c1319c30f0d461465d0c4
Marco Sangalli <msangalli@sapient.com>
2019-01-26 13:12:09 +0100

    adding pre-build js

---
+++ client/build/precache-manifest.b560bc9e03b9cc7ecdc138dcc0b37ee9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2c64b52014bc6547f23c9f10213706d",
+    "url": "/static/media/reconnecting-websocket.e2c64b52.mjs"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c80d89060d4906095fc1",
+    "url": "/static/js/main.c80d8906.chunk.js"
+  },
+  {
+    "revision": "3e8cc50a791b7753ec24",
+    "url": "/static/js/1.3e8cc50a.chunk.js"
+  },
+  {
+    "revision": "c80d89060d4906095fc1",
+    "url": "/static/css/main.c88821a0.chunk.css"
+  },
+  {
+    "revision": "a216e9e56b392fc5f1c2aef41001c2c3",
+    "url": "/index.html"
+  }
+];
